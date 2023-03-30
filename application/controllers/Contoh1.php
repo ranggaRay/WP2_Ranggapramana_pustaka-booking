8a3ab3514bdcd10936c5f8d5fe98e748ece25b13
ranggaRay <ranggabopak18@gmail.com>
2023-03-30 09:59:33 +0800

    push contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</hi>"
+        echo"My name is Rangga Pramana Putra
+             i live in Bojonggede
+             my hobby is playing guitar";
+    }
+}

6dbc78e941ced28d5ccc5b3fb1269e3b2aec2610
ranggaRay <ranggabopak18@gmail.com>
2023-04-06 12:13:37 +0800

    membuat tugas p2

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -1,9 +1,24 @@
 <?php
-public function about()
+defined('BASEPATH') or exit ('no direct script access allowed');
 
- {
- $data['judul'] = "Halaman About";
- $this->load->view('v_header', $data);
- $this->load->view('v_about', $data);
- $this->load->view('v_footer', $data);
- }
+class Web extends CI_Controller{
+
+    function __construct(){
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+    public function index(){
+        $data['judul'] = "Halaman Depan";
+        $this->load->view('v_header',$data);
+        $this->load->view('v_index',$data);
+        $this->load->view('v_footer',$data);
+    }
+    public function about()
+    {
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header',$data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer',$data);
+    }
+}
--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,13 +2,13 @@
  <h1><?php echo $judul ?></h1>
  <h4>Nama</h4>
  <ul type="disc">
- <li>Nama Depan : Rangga Pramana Putra</li>
- <li>Nama Belakang : Rangga</li>
+ <li>Nama Depan : Rangga Pramaan Putra</li>
+ <li>Nama Belakang : Rangga </li>
  </ul>
  <br>
  <h4>Alamat</h4>
  <ul type="none">
- <li> Perumahan Bukit Waringin Blok c 19 no 5</li>
+ <li> Perumahan Bukit Waringin blok c 19 no 5 </li>
  </ul>
  
  <h4>Tempat Lahir</h4>
@@ -17,17 +17,7 @@
  </ul>
  <h4>Olah Raga Favorit</h4>
  <ul type="square">
- <li>Sepak Bola</li>
- <li>Gitar</li>
+ <li>Sepak bola</li>
+ <li>gitar</li>
  </ul>
 </section>
-
-<?php
-public function about()
-
- {
- $data['judul'] = "Halaman About";
- $this->load->view('v_header', $data);
- $this->load->view('v_about', $data);
- $this->load->view('v_footer', $data);
- }
